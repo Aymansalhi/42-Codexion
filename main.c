1984a27e432adc0609d69e23c394faf51fea7056
mirr-x <amir.exe@icloud.com>
2026-07-07 15:42:47 +0100

    refactor main.c thread functions for clarity; update thread messages and structure

--- main.c
+++ main.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <unistd.h>
 
 void *worker(void *arg)
 {
-	printf("Hello from the thread!\n");
+	printf("Hello from the thread! 0 \n");
+	return NULL;
+}
+
+void *worker1(void *arg)
+{
+	printf("Hello from the thread! 1 \n");
 	return NULL;
 }
 
@@ -13,8 +20,16 @@ int main()
 
 	pthread_create(&thread, NULL, worker, NULL);
 
-	pthread_join(thread, NULL);
+	// pthread_join(thread, NULL);
 
+	pthread_t thread1;
+
+	pthread_create(&thread1, NULL, worker1, NULL);
+
+	// pthread_join(thread1, NULL);
+
+	printf("sleeping\n");
+	sleep(10);
 	printf("Main thread finished.\n");
 
 	return 0;
