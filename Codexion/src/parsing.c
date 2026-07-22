51d5f1083b65db9597d056e7ed9f7e6154bceeb3
mirr-x <amir.exe@icloud.com>
2026-07-22 22:35:30 +0100

    refactor: improve argument parsing and validation in ft_parsing_args function

--- Codexion/src/parsing.c
+++ Codexion/src/parsing.c
@@ -6,25 +6,54 @@
 /*   By: mirr <mirr@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/16 20:22:02 by mirr              #+#    #+#             */
-/*   Updated: 2026/07/16 22:09:04 by mirr             ###   ########.fr       */
+/*   Updated: 2026/07/22 16:59:29 by mirr             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "include/codexion.h"
+#include "../include/codexion.h"
 
-t_config	*ft_parsing_args(int arg_len, char **args)
+void	ft_set_config_value(t_config *configs, char *va, int i)
+{
+	if (i == 1)
+		configs->number_of_coders = atoi(va);
+	else if (i == 2)
+		configs->time_to_burnout = atoi(va);
+	else if (i == 3)
+		configs->time_to_compile = atoi(va);
+	else if (i == 4)
+		configs->time_to_debug = atoi(va);
+	else if (i == 5)
+		configs->time_to_refactor = atoi(va);
+	else if (i == 6)
+		configs->number_of_compiles_required = atoi(va);
+	else if (i == 7)
+		configs->dongle_cooldown = atoi(va);
+	else if (i == 8)
+		configs->scheduler = va;
+}
+
+
+t_config	*ft_parsing_args(int argc, char **argv)
 {
 	int			i;
 	t_config	*configs;
 
+	if (argc != 9)
+		return (printf("Error: Invalid number of arguments.\n"), NULL);
+	if (atoi(argv[1]) <= 0 || atoi(argv[2]) <= 0 || atoi(argv[3]) <= 0 ||
+		atoi(argv[4]) <= 0 || atoi(argv[5]) <= 0 || atoi(argv[6]) <= 0 ||
+		atoi(argv[7]) <= 0)
+		return (printf("Error: Invalid argument values.\n"), NULL);
+
 	configs = (t_config *)malloc(sizeof(t_config));
 	if (!configs)
 		return (NULL);
-
-	i = 0;
-	while (i != arg_len)
+	memset(configs, 0, sizeof(t_config));
+	i = 1;
+	while (argc != i)
 	{
-		/* Parsiing Logic*/
+		ft_set_config_value(configs, argv[i], i);
+		i += 1;
 	}
-
+	return (configs);
 }
