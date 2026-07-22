2a4bd119384ca89198bd8b1e524295b10783900c
mirr-x <amir.exe@icloud.com>
2026-07-22 22:35:57 +0100

    refactor: update main.c to improve configuration printing and adjust include path

--- Codexion/src/main.c
+++ Codexion/src/main.c
@@ -6,20 +6,39 @@
 /*   By: mirr <mirr@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/16 20:15:58 by mirr              #+#    #+#             */
-/*   Updated: 2026/07/16 20:39:59 by mirr             ###   ########.fr       */
+/*   Updated: 2026/07/22 16:34:26 by mirr             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
-#include "include/codexion.h"
+#include "../include/codexion.h"
 
 
+void	print_config(t_config *configs)
+{
+	printf("number_of_coders: %d\n", configs->number_of_coders);
+	printf("time_to_burnout: %d\n", configs->time_to_burnout);
+	printf("time_to_compile: %d\n", configs->time_to_compile);
+	printf("time_to_debug: %d\n", configs->time_to_debug);
+	printf("time_to_refactor: %d\n", configs->time_to_refactor);
+	printf(
+		"number_of_compiles_required: %d\n",
+		configs->number_of_compiles_required
+		);
+	printf("dongle_cooldown: %d\n", configs->dongle_cooldown);
+	printf("scheduler: %s\n", configs->scheduler);
+}
 
 int	main(int argc, char **argv)
 {
 	t_config	*configs;
 
+	(void)argc;
 	configs = ft_parsing_args(argc, argv);
+	if (!configs)
+		return (EXIT_FAILURE);
+	print_config(configs);
 
 	free(configs);
+	return (EXIT_SUCCESS);
 }
