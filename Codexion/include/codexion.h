9188e62e842689199847341444f944d0203bc583
mirr-x <amir.exe@icloud.com>
2026-08-10 01:19:50 +0100

    feat: update argument parsing function and add memory cleanup prototypes

--- Codexion/include/codexion.h
+++ Codexion/include/codexion.h
@@ -6,7 +6,7 @@
 /*   By: mirr <mirr@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/15 13:34:07 by mirr              #+#    #+#             */
-/*   Updated: 2026/08/09 23:00:37 by mirr             ###   ########.fr       */
+/*   Updated: 2026/08/10 00:57:21 by mirr             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,10 @@
 # define CODEXION_H
 # define EXIT_FAILURE 1
 # define EXIT_SUCCESS 0
+# define MALLOC_ERROR "Memory allocation failed."
+# define INVALID_ARG "Invalid argument."
+# define INVALD_ARGS "Invalid number of arguments."
+# define INVALID_SCHEDULER "Invalid scheduler. Must be 'fifo' or 'edf'."
 
 //@ -------------------------------------------- INCLUDS ------------
 # include <stdlib.h>
@@ -78,12 +82,20 @@ struct s_queue
 
 
 // @-------------------------------------------- PROTOTYPES ---------
-t_config	*ft_parsing_args(int argc, char **argv);
+int			ft_parsing_args(int argc, char **argv, t_state *state);
 int			create_and_init_coders_and_dongles(t_state *state);
 
 // @-------------------------------------------- PROTOTYPES UTILS -----
 int			ft_strict_int(const char *str, long *out);
 void		ft_set_config_value(t_config *configs, long val, int i);
 int			ft_validate_field(long val, int field_id);
+
+// @-------------------------------------------- PROTOTYPES EXIT_CLEAN -----
+void		clean_memory(t_state *state);
+void		clean_and_print_err(
+				char *err,
+				char *details,
+				int clean,
+				t_state *state);
 
 #endif
