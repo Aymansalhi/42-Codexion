0712de6bdb4de2a5ede7be9bbbbab73dc99d67ba
mirr-x <amir.exe@icloud.com>
2026-08-10 02:01:58 +0100

    fix: improve error handling in create_coders_and_dongles and init_dongles functions

--- Codexion/src/init.c
+++ Codexion/src/init.c
@@ -6,7 +6,7 @@
 /*   By: mirr <mirr@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/08 19:03:48 by mirr              #+#    #+#             */
-/*   Updated: 2026/08/10 00:17:19 by mirr             ###   ########.fr       */
+/*   Updated: 2026/08/10 01:58:20 by mirr             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,12 @@ int	create_coders_and_dongles(t_state *state)
 {
 	state->coders = malloc(sizeof(t_coder) * state->cfg->number_of_coders);
 	if (!state->coders)
-		return (EXIT_FAILURE);
+		return (clean_and_print_err(MALLOC_ERROR, NULL, 1, state),
+			EXIT_FAILURE);
 	state->dongels = malloc(sizeof(t_dongel) * state->cfg->number_of_coders);
 	if (!state->dongels)
-		return (EXIT_FAILURE);
+		return (clean_and_print_err(MALLOC_ERROR, NULL, 1, state),
+			EXIT_FAILURE);
 	return (EXIT_SUCCESS);
 }
 
@@ -32,23 +34,24 @@ int	init_dongles(t_state *state)
 	{
 		state->dongels[i].id = i;
 		state->dongels[i].available = 1;
-		if (!pthread_mutex_init(&state->dongels[i].lock, NULL))
-			return (printf("Mutex initialization failed\n"), EXIT_FAILURE);
+		if (pthread_mutex_init(&state->dongels[i].lock, NULL) != 0)
+			return (clean_and_print_err(MUTEX_DONGEL_ERROR, NULL, 1, state),
+				EXIT_FAILURE);
 		i++;
 	}
 	return (EXIT_SUCCESS);
 }
 
-int	init_coders(t_state *state)
-{
-}
+// int	init_coders(t_state *state)
+// {
+// }
 
 int	create_and_init_coders_and_dongles(t_state *state)
 {
 	if (create_coders_and_dongles(state) == EXIT_FAILURE)
 		return (EXIT_FAILURE);
 
-	if (!init_dongles(state) || !init_coders(state))
+	if (init_dongles(state) == EXIT_FAILURE)
 		return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
 }
