097c5e19543331b9c8f820bee9e78425f0d26202
mirr-x <amir.exe@icloud.com>
2026-08-10 01:19:29 +0100

    feat: refactor memory cleanup and argument parsing in main function

--- Codexion/src/main.c
+++ Codexion/src/main.c
@@ -6,33 +6,25 @@
 /*   By: mirr <mirr@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/16 20:15:58 by mirr              #+#    #+#             */
-/*   Updated: 2026/08/09 23:39:15 by mirr             ###   ########.fr       */
+/*   Updated: 2026/08/10 01:08:05 by mirr             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/codexion.h"
 
-void	clean_memory(t_state *state)
-{
 
-	if (state->coders)
-		free(state->coders);
-	if (state->dongels)
-		free(state->dongels);
-	if (state->cfg)
-		free(state->cfg);
-}
 
 int	main(int argc, char **argv)
 {
 	t_state		state;
 
 	(void)argc;
-	state.cfg = ft_parsing_args(argc, argv);
-	if (!state.cfg)
+	if (ft_parsing_args(argc, argv, &state))
 		return (EXIT_FAILURE);
+	
 	// if (create_and_init_coders_and_dongles(&state) == EXIT_FAILURE)
 	// 	return (EXIT_FAILURE);
 	clean_memory(&state);
+	printf("All memory cleaned successfully. Exiting program.\n");
 	return (EXIT_SUCCESS);
 }
