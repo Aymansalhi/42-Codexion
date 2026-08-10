379d70aaf7df971868f062d965b22ac69df6703b
mirr-x <amir.exe@icloud.com>
2026-08-10 02:01:34 +0100

    fix: update error handling in main function for argument parsing

--- Codexion/src/main.c
+++ Codexion/src/main.c
@@ -6,7 +6,7 @@
 /*   By: mirr <mirr@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/16 20:15:58 by mirr              #+#    #+#             */
-/*   Updated: 2026/08/10 01:08:05 by mirr             ###   ########.fr       */
+/*   Updated: 2026/08/10 01:59:07 by mirr             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,12 @@ int	main(int argc, char **argv)
 
 	(void)argc;
 	if (ft_parsing_args(argc, argv, &state))
+	{
+		clean_memory(&state);
 		return (EXIT_FAILURE);
-	
-	// if (create_and_init_coders_and_dongles(&state) == EXIT_FAILURE)
-	// 	return (EXIT_FAILURE);
+	}
+	if (create_and_init_coders_and_dongles(&state) == EXIT_FAILURE)
+		return (EXIT_FAILURE);
 	clean_memory(&state);
 	printf("All memory cleaned successfully. Exiting program.\n");
 	return (EXIT_SUCCESS);
