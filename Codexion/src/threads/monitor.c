11c74809c58b74d72a2243d8ab91bb56122ccc36
mirr-x <amir.exe@icloud.com>
2026-08-12 11:04:56 +0100

    feat: add monitor thread for tracking coder activity and remove unused simulation module

---
+++ Codexion/src/threads/monitor.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   monitor.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mirr <mirr@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/11 10:55:46 by mirr              #+#    #+#             */
+/*   Updated: 2026/08/11 11:37:31 by mirr             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/codexion.h"
+
+void	*monitor(void *arg)
+{
+	t_state	*state;
+	int		i;
+	int		delete_me;
+
+	state = (t_state *)arg;
+	while (state->simulation_running)
+	{
+		i = 0;
+		while (i < state->cfg->number_of_coders)
+		{
+			printf("monitor: coder %d is working\n", state->coders[i].id);
+			i++;
+		}
+		usleep(100000);
+		if (delete_me == 6)
+			state->simulation_running = 0;
+		delete_me++;
+	}
+	return (NULL);
+}
