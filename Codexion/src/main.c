20f9448e9707494ea34eece8394cb430e104242f
mirr-x <amir.exe@icloud.com>
2026-08-12 11:05:36 +0100

    fix: update simulation state initialization and refactor setup function call

--- Codexion/src/main.c
+++ Codexion/src/main.c
@@ -6,7 +6,7 @@
 /*   By: mirr <mirr@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/16 20:15:58 by mirr              #+#    #+#             */
-/*   Updated: 2026/08/11 10:48:06 by mirr             ###   ########.fr       */
+/*   Updated: 2026/08/11 20:16:27 by mirr             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,15 @@ int	main(int argc, char **argv)
 	t_state		state;
 
 	(void)argc;
+	state.simulation_running = 1;
 	if (ft_parsing_args(argc, argv, &state))
 		return (clean_memory(&state), EXIT_FAILURE);
-	if (init_coders_and_dongles(&state) == EXIT_FAILURE)
+	if (init_and_setup_all(&state) == EXIT_FAILURE)
 		return (EXIT_FAILURE);
 	if (start_simulation(&state) == EXIT_FAILURE) //@TODO: 3 implement the create_threads func
 		return (EXIT_FAILURE);
 
+	state.simulation_running = 0;
 	clean_memory(&state);
 	printf("All memory cleaned successfully. Exiting program.\n");
 	return (EXIT_SUCCESS);
