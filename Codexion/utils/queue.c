73993d442924dbc0cb795e57b8f8e922c1917cc8
mirr-x <amir.exe@icloud.com>
2026-08-12 11:05:09 +0100

    feat: implement queue management functions for coder operations

---
+++ Codexion/utils/queue.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   queue.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mirr <mirr@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/11 19:16:54 by mirr              #+#    #+#             */
+/*   Updated: 2026/08/11 21:48:06 by mirr             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/codexion.h"
+
+void	push_to_queue(t_coder *coder)
+{
+	t_queue	*queue;
+
+	queue = coder->state->queue;
+	pthread_mutex_lock(&coder->state->queue->lock);
+	coder->next = NULL;
+	if (queue->size == 0)
+	{
+		queue->head = coder;
+		queue->tail = coder;
+	}
+	else
+	{
+		queue->tail->next = coder;
+		queue->tail = coder;
+	}
+	queue->size++;
+	pthread_mutex_unlock(&coder->state->queue->lock);
+}
+
+void	pop_from_queue(t_coder *coder)
+{
+	t_queue	*queue;
+
+	queue = coder->state->queue;
+	if (queue->size == 0)
+	{
+		pthread_mutex_unlock(&queue->lock);
+		return ;
+	}
+	queue->head = coder->next;
+	queue->size--;
+	if (queue->size == 0)
+		queue->tail = NULL;
+}
