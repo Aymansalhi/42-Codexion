3c9577bf1528efbf558bb9971c42a69871ec3718
mirr-x <amir.exe@icloud.com>
2026-08-13 12:26:08 +0100

    fix: refactor memory cleanup functions to improve mutex handling and ensure proper resource deallocation

--- Codexion/src/exit_clean.c
+++ Codexion/src/exit_clean.c
@@ -6,24 +6,46 @@
 /*   By: mirr <mirr@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/10 00:26:28 by mirr              #+#    #+#             */
-/*   Updated: 2026/08/10 01:40:48 by mirr             ###   ########.fr       */
+/*   Updated: 2026/08/12 11:34:46 by mirr             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/codexion.h"
 
+
+void	clean_and_destroy_mutexes(t_state *state)
+{
+	int		i;
+
+	i = 0;
+	if (state->dongels)
+	{
+		while (i < state->dongels_initialized)
+		{
+			pthread_mutex_destroy(&state->dongels[i].lock);
+			i++;
+		}
+	}
+	if (state->queue)
+		pthread_mutex_destroy(&state->queue->lock);
+}
+
 void	clean_memory(t_state *state)
 {
-
+	pthread_cond_destroy(&state->coder_wait_cond);
+	clean_and_destroy_mutexes(state);
 	if (state->coders)
 		free(state->coders);
 	if (state->dongels)
 		free(state->dongels);
 	if (state->cfg)
 		free(state->cfg);
+	if (state->queue)
+		free(state->queue);
 	state->coders = NULL;
 	state->dongels = NULL;
 	state->cfg = NULL;
+	state->queue = NULL;
 }
 
 void	clean_and_print_err(char *err, char *details, int clean, t_state *state)
