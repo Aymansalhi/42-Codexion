bb814cce568aa04d04b49c8d24bb92002d72782f
mirr-x <amir.exe@icloud.com>
2026-08-13 12:25:58 +0100

    fix: update timestamp in header and ensure state initialization before simulation starts

--- Codexion/src/main.c
+++ Codexion/src/main.c
@@ -6,7 +6,7 @@
 /*   By: mirr <mirr@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/16 20:15:58 by mirr              #+#    #+#             */
-/*   Updated: 2026/08/11 20:16:27 by mirr             ###   ########.fr       */
+/*   Updated: 2026/08/12 12:06:53 by mirr             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,13 @@ int	main(int argc, char **argv)
 	t_state		state;
 
 	(void)argc;
+	memset(&state, 0, sizeof(t_state));
 	state.simulation_running = 1;
 	if (ft_parsing_args(argc, argv, &state))
 		return (clean_memory(&state), EXIT_FAILURE);
 	if (init_and_setup_all(&state) == EXIT_FAILURE)
 		return (EXIT_FAILURE);
-	if (start_simulation(&state) == EXIT_FAILURE) //@TODO: 3 implement the create_threads func
+	if (start_simulation(&state) == EXIT_FAILURE) //@TODO: 0 implement the create_threads func
 		return (EXIT_FAILURE);
 
 	state.simulation_running = 0;
