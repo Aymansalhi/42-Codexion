291c824587de0fbc9d134041f80fcdecb80a8685
mirr-x <amir.exe@icloud.com>
2026-08-16 14:16:57 +0100

    fix: implement set_burnout function in coder_utils.c for managing burnout timing

--- Codexion/utils/coder_utils.c
+++ Codexion/utils/coder_utils.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   coder_utilities.c                                  :+:      :+:    :+:   */
+/*   coder_utils.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mirr <mirr@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/14 00:21:13 by mirr              #+#    #+#             */
-/*   Updated: 2026/08/14 00:27:26 by mirr             ###   ########.fr       */
+/*   Updated: 2026/08/15 16:09:12 by mirr             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 void	set_burnout(t_coder *coder)
 {
 	pthread_mutex_lock(&coder->mutex_burnout);
-	coder->time_bournout = get_time_ms();
+	coder->time_burnout = get_time_in_ms();
 	pthread_mutex_unlock(&coder->mutex_burnout);
 }
+
