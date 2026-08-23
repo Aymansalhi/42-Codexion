4b6fe33ee7f29e69afe611253d9031bd261ccaa4
mirr-x <amir.exe@icloud.com>
2026-08-23 01:07:34 +0100

    feat: add priority queue implementation with deadline management and node swapping

---
+++ Codexion/src/queue/priority_queue3.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   priority_queue3.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: molahrac <molahrac@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/22 23:35:07 by molahrac          #+#    #+#             */
+/*   Updated: 2026/08/22 23:39:54 by molahrac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/codexion.h"
+
+static long long	get_deadline(t_coder *coder)
+{
+	return (coder->last_compile_start + coder->state->cfg->time_to_burnout);
+}
+
+static int	compare_by_id(t_coder *a, t_coder *b)
+{
+	if (a->id < b->id)
+		return (-1);
+	if (a->id > b->id)
+		return (1);
+	return (0);
+}
+
+static void	swap_heap_nodes(t_priority_queue *queue, int a, int b)
+{
+	t_coder	*tmp;
+
+	tmp = queue->heap[a];
+	queue->heap[a] = queue->heap[b];
+	queue->heap[b] = tmp;
+	queue->heap[a]->queue_index = a;
+	queue->heap[b]->queue_index = b;
+}
