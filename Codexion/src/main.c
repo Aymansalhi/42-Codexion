0274ac599643b6537555aa475f2d0fb6fe0cb329
mirr-x <amir.exe@icloud.com>
2026-08-28 12:06:40 +0100

    style: update author information in main.c header comments refactor: streamline simulation flow by stopping simulation before cleanup

--- Codexion/src/main.c
+++ Codexion/src/main.c
@@ -3,16 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mirr <mirr@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: molahrac <molahrac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/07/16 20:15:58 by mirr              #+#    #+#             */
-/*   Updated: 2026/08/19 14:11:57 by mirr             ###   ########.fr       */
+/*   Created: 2026/07/16 20:15:58 by molahrac          #+#    #+#             */
+/*   Updated: 2026/08/22 02:06:18 by molahrac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/codexion.h"
-
-
 
 int	main(int argc, char **argv)
 {
@@ -21,16 +19,16 @@ int	main(int argc, char **argv)
 	(void)argc;
 	memset(&state, 0, sizeof(t_state));
 	state.simulation_running = 1;
+	if (pthread_mutex_init(&state.state_lock, NULL) != 0)
+		return (EXIT_FAILURE);
+	state.state_lock_initialized = 1;
 	if (ft_parsing_args(argc, argv, &state))
 		return (clean_memory(&state), EXIT_FAILURE);
 	if (init_and_setup_all(&state) == EXIT_FAILURE)
 		return (EXIT_FAILURE);
 	if (start_simulation(&state) == EXIT_FAILURE)
 		return (EXIT_FAILURE);
-
-	state.simulation_running = 0;
+	stop_simulation(&state);
 	clean_memory(&state);
 	return (EXIT_SUCCESS);
 }
-
-// @TODO : implement EDF scheduler
